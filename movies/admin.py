38ce03befd9f9f8912b52e225ea9c117950795bb
ikenshu <undefined.hlo.o@gmail.com>
2016-08-15 10:13:46 -0400

    Definiendo los modelos

---
+++ movies/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Movie, Categorie, Genre, Clasification
+
+admin.site.register(Movie)
+admin.site.register(Categorie)
+admin.site.register(Genre)
+admin.site.register(Clasification)
---
+++ movies/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+class Categorie(models.Model):
+	name = models.CharField(max_length=255)
+	description = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	modified_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.name
+
+class Genre(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	modified_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.name
+
+class Clasification(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	modified_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.name
+
+class Movie(models.Model):
+	title = models.CharField(max_length=255)
+	poster = models.ImageField(upload_to='static/movies/img')
+	description = models.TextField()
+	duration = models.CharField(max_length=255)
+	genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+	categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+	clasification = models.ForeignKey(Clasification, on_delete=models.CASCADE)
+	created_at = models.DateTimeField(auto_now_add=True)
+	modified_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title
+
